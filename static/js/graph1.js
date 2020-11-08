64d7eed29d879eb18d7244cd584facb872553db9
Kurt Pessa <kpessa@gmail.com>
2020-11-07 20:32:25 -0500

    udpated

--- static/js/graph1.js
+++ static/js/graph1.js
@@ -1,15 +1,20 @@
 d3.select("#plot1").text("HELLLLOOO");
 
+// Model graph
 d3.csv("static/data/graph_1.csv").then((data) => {
   // console.log(data);
   let x = data.map((d) => +d[""]);
   let y = data.map((d) => +d.opening_price);
 
-  var trace1 = {
+  var trace_actual = {
     x: x,
     y: y,
     type: "scatter",
   };
 
-  Plotly.newPlot("plot1", [trace1]);
+  var trace_predictions = {};
+
+  Plotly.newPlot("plot1", [trace_actual]);
 });
+
+// Forecasting Graph
